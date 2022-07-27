5acc26c327ba917658ef4cc67969758ac9e11f53
Ygnas <ygnas@outlook.com>
2022-07-27 14:29:50 +0100

    Utility for conversion of data

--- utils/conversion.js
+++ utils/conversion.js
@@ -1,8 +1,22 @@
 "use strict";
 
+const weatherCodes = {
+    100: "Clear",
+    200: "Partial Clouds",
+    300: "Cloudy",
+    400: "Light Showers",
+    500: "Heavy Showers",
+    600: "Rain",
+    700: "Snow",
+    800: "Thunder"
+};
+
 const conversion = {
     tempF(tempC) {
         return (tempC * 1.8) + 32;
+    },
+    currentWeather(code) {
+        return weatherCodes[code];
     }
 };
 
