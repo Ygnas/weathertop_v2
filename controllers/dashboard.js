0b70f9fd1b66c1e25bad8406a79f96942a2b6e9c
Ygnas <ygnas@outlook.com>
2022-07-29 20:27:39 +0100

    Ability to add new station

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,16 +2,26 @@
 
 const logger = require("../utils/logger");
 const stationCollection = require("../models/station-store.js");
+const uuid = require("uuid");
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "WeatherTop Dashboard",
-      stations: stationCollection,
+      stations: stationCollection.getAllStation(),
     };
-    logger.info("about to render", stationCollection);
+    logger.info("about to render", stationCollection.getAllStation());
     response.render("dashboard", viewData);
+  },
+  addStation(request, response) {
+    const newStation = {
+        id: uuid.v1(),
+        name: request.body.name,
+        readings: []
+    }
+    stationCollection.addStation(newStation);
+    response.redirect("/dashboard");
   }
 };
 
