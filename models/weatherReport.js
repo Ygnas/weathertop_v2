630fc83b35037c52343bddae2a87ef533f1dfbf2
Ygnas <ygnas@outlook.com>
2022-07-29 20:29:23 +0100

    Updated weather report

--- models/weatherReport.js
+++ models/weatherReport.js
@@ -1,12 +1,14 @@
 "use strict";
 
-const stationCollection = require("./station-store.json").stationCollection;
+const stationCollection = require("./station-store.js");
 const conversion = require("../utils/conversion");
 
 const weatherReport = {
-    WeatherReport : function(id) {
-        const station = stationCollection.find(station => station.id == id);
+    WeatherReport : function(stationId) {
+        const station = stationCollection.getStation(stationId);
         const latestReading = station.readings[station.readings.length-1];
+        this.name = station.name;
+        if (typeof latestReading == 'undefined') return 0;
         this.code = latestReading.code;
         this.tempC = latestReading.temperature + " C";
         this.windSpeed = latestReading.windSpeed + " bft";
