d1424f080fe8e3b4828df802174188994ca5653a
Ygnas <ygnas@outlook.com>
2022-08-11 19:53:25 +0100

    Min max values for temp, wind and pressure

--- models/weatherReport.js
+++ models/weatherReport.js
@@ -14,10 +14,16 @@ const weatherReport = {
         this.code = latestReading.code;
         this.icon = conversion.weatherIcon(this.code);
         this.tempC = latestReading.temperature + " C";
+        this.maxTemp = "Max: " + conversion.maxTemp(station.readings);
+        this.minTemp = "Min: " + conversion.minTemp(station.readings);
         this.beafourt = conversion.beafourt(latestReading.windSpeed) + " bft";
         this.windCompass = conversion.degreesToCompass(latestReading.windDirection);
         this.windChill = "Feels like \n" + conversion.windChill(latestReading.temperature, latestReading.windSpeed);
+        this.maxWind = "Max: " + conversion.maxWind(station.readings);;
+        this.minWind = "Min: " + conversion.minWind(station.readings);;
         this.pressure = latestReading.pressure + " hpa";
+        this.maxPressure = "Max: " + conversion.maxPressure(station.readings);
+        this.minPressure = "Min: " + conversion.minPressure(station.readings);
         this.tempF = conversion.tempF(latestReading.temperature) + " F";
         this.weather = conversion.currentWeather(this.code);
     }
